f374664f16cb3ecc6cffb15b1b3383b7b040d4d6
Yan Xue <3491507+crazyxy@users.noreply.github.com>
2019-06-25 10:41:36 -0700

    admin: add host priority to cluster response (#7317)
    Add host priority to cluster response in admin server.
    
    Risk Level: low
    Testing: unit test
    Docs Changes: N/A
    Release Notes: updated
    
    Signed-off-by: Yan Xue <yxyan@google.com>
    
    Signed-off-by: crazyxy <yxyan@google.com>

--- source/server/http/admin.cc
+++ source/server/http/admin.cc
@@ -388,6 +388,8 @@ void AdminImpl::writeClustersAsJson(Buffer::Instance& response) {
         }
 
         host_status.set_weight(host->weight());
+
+        host_status.set_priority(host->priority());
       }
     }
   }
@@ -443,6 +445,8 @@ void AdminImpl::writeClustersAsText(Buffer::Instance& response) {
         response.add(fmt::format("{}::{}::success_rate::{}\n", cluster.second.get().info()->name(),
                                  host->address()->asString(),
                                  host->outlierDetector().successRate()));
+        response.add(fmt::format("{}::{}::priority::{}\n", cluster.second.get().info()->name(),
+                                 host->address()->asString(), host->priority()));
       }
     }
   }
